4dd2bbb9dd3d6f8be04354bba21534835776e0cd
Dice-K39 <daisukehashimoto39@gmail.com>
2020-12-26 14:00:17 -0500

    Uploading to github.

---
+++ 05 ActiveRecord/sinatra-ar-crud-lab-onl01-seng-pt-041320/app/controllers/application_controller.rb
@@ -0,0 +1,55 @@
+
+require_relative '../../config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+  end
+
+  get "/articles/new" do
+    erb :new
+  end
+
+  post "/articles" do
+    article = Article.create(title: params["title"], content: params["content"])
+
+    redirect "/articles/#{Article.last.id}"
+  end
+
+  get "/articles" do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+    Article.update(params[:id], title: params[:title], content: params[:content])
+    
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  delete "/articles/:id" do
+    Article.delete(params[:id])
+
+    redirect "/articles"
+  end
+end
